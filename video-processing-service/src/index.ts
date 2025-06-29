3e6616960df6716ad61a51f2239192c5e6bd3ccf
Jackie <jlin226859643@gmail.com>
2025-06-28 20:55:07 -0400

    Initial commit

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,34 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+ffmpeg.setFfmpegPath("/usr/bin/ffmpeg"); // This is the path in most Ubuntu/WSL setups
 
 const app = express()
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req,res) => { //http get endpoint
-    res.send("Hello World!")
+
+app.post("/process-video", (req,res) => { //http post endpoint
+    //get path of input video file from the req body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if(!inputFilePath || !outputFilePath){
+        res.status(400).send("Bad Request: Missing file path.");
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions('-vf', "scale=-1:360") //360p
+        .on("end", ()=> {
+            res.status(200).send("Processing finished successfully.")
+        })
+        .on("error", (err) =>{
+            console.log(`An error occured: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`)
+        })
+        .save(outputFilePath);
+    
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log(`Video processing service listening at http://localhost:${port}`)
 });
